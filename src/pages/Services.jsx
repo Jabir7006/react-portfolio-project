a0affd2d4e8d473e9a11e377b55f74cbbd88d289
Jabir7006 <www.roman1249@gmail.com>
2023-08-07 09:53:13 +0600

    updated

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,14 +4,12 @@ import { BsCode } from "react-icons/bs";
 import { BiTrendingUp } from "react-icons/bi";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { LuVerified } from "react-icons/lu";
-import Skills from "./skills";
-
+import Skills from "./Skills";
 
 const Services = () => {
-
-const [firstModal, setFirstModal] = useState(false);
-const [thirdModal, setThirdModal] = useState(false);
-const [secondModal, setSecondModal] = useState(false);
+  const [firstModal, setFirstModal] = useState(false);
+  const [thirdModal, setThirdModal] = useState(false);
+  const [secondModal, setSecondModal] = useState(false);
 
   return (
     <section className="bg-[#0b0a10]">
@@ -28,7 +26,10 @@ const [secondModal, setSecondModal] = useState(false);
             <MdWeb size={22} className="text-[#ff3c4c] mb-2" />
             <p className="text-[.98rem] pb-3 md:font-medium">Web Designer</p>
 
-            <span className="group flex items-center gap-2 cursor-pointer" onClick={() => setFirstModal(true)}>
+            <span
+              className="group flex items-center gap-2 cursor-pointer"
+              onClick={() => setFirstModal(true)}
+            >
               <p className="text-[.8rem] text-[#f86874] ">View More</p>
               <AiOutlineArrowRight
                 size={13}
@@ -40,7 +41,10 @@ const [secondModal, setSecondModal] = useState(false);
             <BsCode size={22} className="text-[#ff3c4c] mb-2" />
             <p className="text-[.98rem] pb-3 md:font-medium">UI/UX Designer</p>
 
-            <span className="group flex items-center gap-2 cursor-pointer" onClick={() => setSecondModal(true)}>
+            <span
+              className="group flex items-center gap-2 cursor-pointer"
+              onClick={() => setSecondModal(true)}
+            >
               <p className="text-[.8rem] text-[#f86874] ">View More</p>
               <AiOutlineArrowRight
                 size={13}
@@ -51,7 +55,10 @@ const [secondModal, setSecondModal] = useState(false);
           <div className="bg-[#18181e] p-8 rounded flex flex-col items-center min-[425px]:py-12 lg:items-start lg:w-56 lg:h-64 lg:justify-end">
             <BiTrendingUp size={22} className="text-[#ff3c4c] mb-2" />
             <p className="text-[.98rem] pb-3 md:font-medium">Branding Designer</p>
-            <span className="group flex items-center gap-2 cursor-pointer" onClick={() => setThirdModal(true)}>
+            <span
+              className="group flex items-center gap-2 cursor-pointer"
+              onClick={() => setThirdModal(true)}
+            >
               <p className="text-[.8rem] text-[#f86874] ">View More</p>
               <AiOutlineArrowRight
                 size={13}
@@ -63,144 +70,129 @@ const [secondModal, setSecondModal] = useState(false);
 
         {/* ===== Service Details ===== */}
 
-        {
-          firstModal && (
-        
-           <section className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black z-50 mx-3 md:mx-0">
-
-            
-
-             <div className="modal relative bg-[#18161f] p-8 w-full rounded-xl max-w-md">
-              
+        {firstModal && (
+          <section className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black z-50 mx-3 md:mx-0">
+            <div className="modal relative bg-[#18161f] p-8 w-full rounded-xl max-w-md">
               <button className="absolute right-3 top-3" onClick={() => setFirstModal(false)}>
-                <MdOutlineClose size={20} className="text-[#ff3c4c]"/>
+                <MdOutlineClose size={20} className="text-[#ff3c4c]" />
               </button>
-  
-            <h2 className="text-center mb-3">Web Designer</h2>
-            <p className="text-[.87rem] text-[#cdd0dd] text-center mb-8">
-              Services with more than 3 years of experience. Providing quality work to clients and
-              compor
-            </p>
-  
-            <ul className="list-none flex flex-col gap-4">
-              <li className="flex items-center gap-2">
-                <LuVerified size={20} className=" text-[#ff3c4c]"/>
-                <p className="text-[.84rem] text-[#cdd0dd]">I develop the user interface.</p></li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-               <p className="text-[.84rem] text-[#cdd0dd]"> Web page development.</p>
-               </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-                <p className="text-[.84rem] text-[#cdd0dd]">I create ux element interactions.</p>
+
+              <h2 className="text-center mb-3">Web Designer</h2>
+              <p className="text-[.87rem] text-[#cdd0dd] text-center mb-8">
+                Services with more than 3 years of experience. Providing quality work to clients and
+                compor
+              </p>
+
+              <ul className="list-none flex flex-col gap-4">
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">I develop the user interface.</p>
                 </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-               <p className="text-[.84rem] text-[#cdd0dd]"> I position your company brand.</p>
-               </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className="text-[#ff3c4c]"/>
-              <p className="text-[.84rem] text-[#cdd0dd]"> Design and mockups of products for companies.</p>
-              </li>
-            </ul>
-          </div>
-         
-           </section>
-           
-          )
-        }
-        {
-          secondModal && (
-        
-           <section className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black z-50">
-
-            
-
-             <div className="modal relative bg-[#18161f] p-8 w-full rounded-xl max-w-md">
-              
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]"> Web page development.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">I create ux element interactions.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]"> I position your company brand.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className="text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">
+                    {" "}
+                    Design and mockups of products for companies.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </section>
+        )}
+        {secondModal && (
+          <section className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black z-50">
+            <div className="modal relative bg-[#18161f] p-8 w-full rounded-xl max-w-md">
               <button className="absolute right-3 top-3" onClick={() => setSecondModal(false)}>
-                <MdOutlineClose size={20} className="text-[#ff3c4c]"/>
+                <MdOutlineClose size={20} className="text-[#ff3c4c]" />
               </button>
-  
-            <h2 className="text-center mb-3">UI/UX Designer</h2>
-            <p className="text-[.87rem] text-[#cdd0dd] text-center mb-8">
-              Services with more than 3 years of experience. Providing quality work to clients and
-              compor
-            </p>
-  
-            <ul className="list-none flex flex-col gap-4">
-              <li className="flex items-center gap-2">
-                <LuVerified size={20} className=" text-[#ff3c4c]"/>
-                <p className="text-[.84rem] text-[#cdd0dd]">I develop the user interface.</p></li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-               <p className="text-[.84rem] text-[#cdd0dd]"> Web page development.</p>
-               </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-                <p className="text-[.84rem] text-[#cdd0dd]">I create ux element interactions.</p>
+
+              <h2 className="text-center mb-3">UI/UX Designer</h2>
+              <p className="text-[.87rem] text-[#cdd0dd] text-center mb-8">
+                Services with more than 3 years of experience. Providing quality work to clients and
+                compor
+              </p>
+
+              <ul className="list-none flex flex-col gap-4">
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">I develop the user interface.</p>
                 </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-               <p className="text-[.84rem] text-[#cdd0dd]"> I position your company brand.</p>
-               </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className="text-[#ff3c4c]"/>
-              <p className="text-[.84rem] text-[#cdd0dd]"> Design and mockups of products for companies.</p>
-              </li>
-            </ul>
-          </div>
-         
-           </section>
-           
-          )
-        }
-        {
-          thirdModal && (
-        
-           <section className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black z-50">
-
-            
-
-             <div className="modal relative bg-[#18161f] p-8 w-full rounded-xl max-w-md">
-              
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]"> Web page development.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">I create ux element interactions.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]"> I position your company brand.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className="text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">
+                    {" "}
+                    Design and mockups of products for companies.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </section>
+        )}
+        {thirdModal && (
+          <section className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-black z-50">
+            <div className="modal relative bg-[#18161f] p-8 w-full rounded-xl max-w-md">
               <button className="absolute right-3 top-3" onClick={() => setThirdModal(false)}>
-                <MdOutlineClose size={20} className="text-[#ff3c4c]"/>
+                <MdOutlineClose size={20} className="text-[#ff3c4c]" />
               </button>
-  
-            <h2 className="text-center mb-3">Branding Designer</h2>
-            <p className="text-[.87rem] text-[#cdd0dd] text-center mb-8">
-              Services with more than 3 years of experience. Providing quality work to clients and
-              compor
-            </p>
-  
-            <ul className="list-none flex flex-col gap-4">
-              <li className="flex items-center gap-2">
-                <LuVerified size={20} className=" text-[#ff3c4c]"/>
-                <p className="text-[.84rem] text-[#cdd0dd]">I develop the user interface.</p></li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-               <p className="text-[.84rem] text-[#cdd0dd]"> Web page development.</p>
-               </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-                <p className="text-[.84rem] text-[#cdd0dd]">I create ux element interactions.</p>
+
+              <h2 className="text-center mb-3">Branding Designer</h2>
+              <p className="text-[.87rem] text-[#cdd0dd] text-center mb-8">
+                Services with more than 3 years of experience. Providing quality work to clients and
+                compor
+              </p>
+
+              <ul className="list-none flex flex-col gap-4">
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">I develop the user interface.</p>
                 </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className=" text-[#ff3c4c]"/>
-               <p className="text-[.84rem] text-[#cdd0dd]"> I position your company brand.</p>
-               </li>
-              <li className="flex items-center gap-2">
-              <LuVerified size={20} className="text-[#ff3c4c]"/>
-              <p className="text-[.84rem] text-[#cdd0dd]"> Design and mockups of products for companies.</p>
-              </li>
-            </ul>
-          </div>
-         
-           </section>
-           
-          )
-        }
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]"> Web page development.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">I create ux element interactions.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className=" text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]"> I position your company brand.</p>
+                </li>
+                <li className="flex items-center gap-2">
+                  <LuVerified size={20} className="text-[#ff3c4c]" />
+                  <p className="text-[.84rem] text-[#cdd0dd]">
+                    {" "}
+                    Design and mockups of products for companies.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </section>
+        )}
 
         <Skills />
       </div>
